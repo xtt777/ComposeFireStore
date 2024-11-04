a086a97870174eeb0e918e55f4aee1609aca82c2
xtt777 <xxtong28@gmail.com>
2024-11-04 17:06:03 +0800

    v12刪除資料

--- app/src/main/java/com/example/composefirestore/MainActivity.kt
+++ app/src/main/java/com/example/composefirestore/MainActivity.kt
@@ -141,7 +141,12 @@ fun Birth(m: Modifier){
             }) {
                 Text("查詢資料")
             }
-            Button(onClick = {  }) {
+            Button(onClick = {
+                db.collection("users")
+                  .document(userName)
+                  .delete()
+                msg = "刪除資料"
+            }) {
                 Text("刪除資料")
             }
         }
