9c34871da671d265937575a1e0931f9987d43339
xtt777 <xxtong28@gmail.com>
2024-11-04 17:00:49 +0800

    v9查詢最重之兩人資料

--- app/src/main/java/com/example/composefirestore/MainActivity.kt
+++ app/src/main/java/com/example/composefirestore/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.ui.text.input.KeyboardType
 import com.google.firebase.Firebase
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.firestore
 
 class MainActivity : ComponentActivity() {
@@ -120,7 +121,9 @@ fun Birth(m: Modifier){
             Button(onClick = {
                 db.collection("users")
                    // .whereEqualTo("userName", userName)
-                    .whereLessThan("userWeight", userWeight)
+                    //.whereLessThan("userWeight", userWeight)
+                    .orderBy("userWeight", Query.Direction.DESCENDING)
+                    .limit(2)
                     .get()
                     .addOnCompleteListener { task ->
                         if (task.isSuccessful) {
