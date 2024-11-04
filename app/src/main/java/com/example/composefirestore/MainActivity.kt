b68f95d7824d556be5ac80b3ffa436c3149c47c3
xtt777 <xxtong28@gmail.com>
2024-11-04 16:18:47 +0800

    v6firestore設定

--- app/src/main/java/com/example/composefirestore/MainActivity.kt
+++ app/src/main/java/com/example/composefirestore/MainActivity.kt
@@ -52,6 +52,8 @@ fun Birth(m: Modifier){
     var userName by remember { mutableStateOf("郭芯彤")}
     var userWeight by remember { mutableStateOf(3800)}
     var userPassword by remember { mutableStateOf("")}
+    var msg by remember { mutableStateOf("訊息")}
+
 
 
     Column {
@@ -94,6 +96,19 @@ fun Birth(m: Modifier){
 
         Text("您輸入的姓名是：$userName\n出生體重為：$userWeight 公克"
                 + "\n密碼：$userPassword")
+
+        Row {
+            Button(onClick = {  }) {
+                Text("新增/修改資料")
+            }
+            Button(onClick = {  }) {
+                Text("查詢資料")
+            }
+            Button(onClick = {  }) {
+                Text("刪除資料")
+            }
+        }
+        Text(text = msg)
 
     }
 
